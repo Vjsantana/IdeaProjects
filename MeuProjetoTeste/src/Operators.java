555d85099cd1130e3440581de40baeb22f871045
Vjsantana <72237096+Vjsantana@users.noreply.github.com>
2021-06-29 19:35:33 -0300

    Update Class

---
+++ MeuProjetoTeste/src/Operators.java
@@ -0,0 +1,21 @@
+public class Operators {
+
+    public static void main(String[] args) {
+
+        int valor1 = 10;
+        int valor2 = 20;
+        int somaDosValores = valor1+valor2;
+        int diferencaDosValores = valor2-valor1;
+        int produtoDosValores = valor1*valor2;
+        int divisaoDosValores = valor2/valor1;
+
+        System.out.println("A soma dos valores informados é "+somaDosValores);
+
+        System.out.println("A diferença dos valores informados é "+diferencaDosValores);
+
+        System.out.println("O produto dos valores informados é "+produtoDosValores);
+
+        System.out.println("A divisão dos valores informados é "+divisaoDosValores);
+
+    }
+}
