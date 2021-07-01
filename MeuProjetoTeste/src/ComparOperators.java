d61885e21581b4046e71892e68199ea67dfad426
Vjsantana <72237096+Vjsantana@users.noreply.github.com>
2021-07-01 17:20:14 -0300

    Compars Operators
    New Class in Java.

---
+++ MeuProjetoTeste/src/ComparOperators.java
@@ -0,0 +1,30 @@
+public class ComparOperators {
+
+    public static void main(String[] args) {
+
+        boolean lessThan = 15 < 20;
+
+        boolean biggerThan = 15 > 20;
+
+        boolean lessThanOrEgualTo = 15 <= 20;
+
+        boolean greaterThanOrEqualTo = 15 >= 20;
+
+        boolean equalTo = 15 == 20;
+
+        boolean differentThan = 15 != 20;
+
+        System.out.println("15 é menor que 20 = "+lessThan);
+
+        System.out.println("15 é maior que 20 = "+biggerThan);
+
+        System.out.println("15 é menor ou igual a 20 = "+lessThanOrEgualTo);
+
+        System.out.println("15 é maior ou igual a 20 = "+greaterThanOrEqualTo);
+
+        System.out.println("15 é igual a 20 = "+equalTo);
+
+        System.out.println("15 é diferente de 20 = "+differentThan);
+
+    }
+}
